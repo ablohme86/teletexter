af10c8eba0a4f7bdc5c612ef364a4c42089d0509
Alexander Blohme <alexander@thrino.no>
2024-06-12 23:24:44 +0200

    first working commit

--- lcdlib.c
+++ lcdlib.c
@@ -1,3 +1,28 @@
+/******************************************************************************
+ *                                                                            *
+ *                               TeleTexter                                   *
+ *                                                                            *
+ *                Developed by: Alexander Blohme                              *
+ *                Initial Release: 2024                                       *
+ *                                                                            *
+ *   lcdlib.c - For handling the i2c lcd display                              *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the               *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.      *
+ *                                                                            *
+ ******************************************************************************/
+
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -17,7 +42,6 @@ void delay(int milliseconds)
 
 void i2c_init(const char *device) 
 {
-
     i2c_bus = open(device, O_RDWR);
     if (i2c_bus < 0) 
     {
@@ -29,31 +53,36 @@ void i2c_init(const char *device)
         perror("Failed to acquire bus access and/or talk to slave");
         exit(1);
     }
-    // send init params
-   write_command(0x33);
-   write_command(0x32);
-   write_command(0x06);
-   write_command(0x0C);
-   write_command(0x28);
- 
+    
+    // Send init params
+    write_command(0);
+    write_command(0x33);
+    write_command(0x32);
+    write_command(0x06);
+    write_command(0x0C);
+    write_command(0x28);
+    
+    // Clear LCD screen
+    lcd_clear();
+    
+    delay(10); // Optional delay for stabilization
 }
+
 
 void i2c_write_byte(uint8_t byte) 
 {
-
     if (write(i2c_bus, &byte, 1) != 1) 
     {
         fprintf(stderr, "Failed to write to the i2c bus\n");
         exit(1);
     }
-   printf("%d bytes written to LCD!\n", byte);
 }
 
 void write_command(uint8_t byte) 
 {
     i2c_write_byte(byte);
     i2c_write_byte(byte | ENABLE_BIT);
-    delay(10);
+    delay(1);
     i2c_write_byte(byte & ~ENABLE_BIT);
     delay(10);
 }
@@ -67,97 +96,77 @@ void lcd_write(uint8_t byte, uint8_t mode)
     }
     write_command(mode | (byte & 0xF0) | backlight_mode);
     write_command(mode | ((byte << 4) & 0xF0) | backlight_mode);
+   
 }
 
 void lcd_text(const char *text, uint8_t line, uint8_t align) 
 {
-    uint8_t lcd_line;
-    switch(line) 
-    {
-        case 1:
-            lcd_line = LINE_1;
-            break;
-        case 2:
-            lcd_line = LINE_2;
-            break;
-        case 3:
-            lcd_line = LINE_3;
-            break;
-        case 4:
-            lcd_line = LINE_4;
-            break;
-        default:
-            lcd_line = LINE_1;
-            break;
-    }
 
-    lcd_write(lcd_line, 0);
-    int text_len = strlen(text);
-    char *aligned_text = (char *)malloc(LCD_WIDTH + 1);
-    if (aligned_text == NULL) 
-    {
-        fprintf(stderr, "Memory allocation failed\n");
-        exit(1);
-    }
-    if (align == LEFT) 
-    {
-        snprintf(aligned_text, LCD_WIDTH + 1, "%-16.16s", text);
-    } 
-    else if (align == RIGHT) 
-    {
-        snprintf(aligned_text, LCD_WIDTH + 1, "%16.16s", text);
-    } 
-    else if (align == CENTER) 
-    {
-        int padding = (LCD_WIDTH - text_len) / 2;
-        snprintf(aligned_text, LCD_WIDTH , "%*.*s%s%*.*s", padding, padding, "", text, LCD_WIDTH - padding - text_len, LCD_WIDTH - padding - text_len, "");
-    }
-    for (int i = 0; i < LCD_WIDTH; i++) 
-    {
-        lcd_write(aligned_text[i], 1);
-    }
-    free(aligned_text);
+     uint8_t lcd_line;
+     switch(line) 
+     {
+         case 1:
+             lcd_line = LINE_1;
+             break;
+         case 2:
+             lcd_line = LINE_2;
+             break;
+         case 3:
+             lcd_line = LINE_3;
+             break;
+         case 4:
+             lcd_line = LINE_4;
+             break;
+         default:
+             lcd_line = LINE_1;
+             break;
+     }
+     
+     lcd_write(lcd_line, 0);
+     
+     // Oppdater tekstlinjen på LCD-skjermen
+     static char lcd_lines[LCD_ROWS][LCD_WIDTH + 1] = {0}; // Lagrer teksten på hver linje
+     if (align == LEFT) 
+     {
+         snprintf(lcd_lines[line - 1], LCD_WIDTH + 1, "%-.*s", LCD_WIDTH, text);
+     } 
+     else if (align == RIGHT) 
+     {
+         snprintf(lcd_lines[line - 1], LCD_WIDTH + 1, "%*.*s", LCD_WIDTH, LCD_WIDTH, text);
+     } 
+     else if (align == CENTER) 
+     {
+         int padding = (LCD_WIDTH - strlen(text)) / 2;
+         snprintf(lcd_lines[line - 1], LCD_WIDTH + 1, "%*.*s%s%*.*s", padding, padding, "", text, LCD_WIDTH - padding - strlen(text), LCD_WIDTH - padding - strlen(text), "");
+     }
+     
+     // Fyll resten av linjen med mellomrom
+     int text_len = strlen(lcd_lines[line - 1]);
+     for (int i = text_len; i < LCD_WIDTH; i++) 
+     {
+         lcd_lines[line - 1][i] = ' ';
+     }
+     
+     for (int i = 0; i < LCD_WIDTH; i++) 
+     {
+         lcd_write(lcd_lines[line - 1][i], 1);
+     }
 }
 
 void lcd_backlight(uint8_t turn_on) 
 {
     if (turn_on) 
     {
-        lcd_write(0, 1);
+        write_command(0);
     } 
     else 
     {
-        lcd_write(LCD_NOBACKLIGHT, 1);
+        write_command(LCD_NOBACKLIGHT);
     }
 }
 
 void lcd_clear() 
 {
-
-
-	// prøv denne koden når kommer hjem..
-    write_command(CLEAR_DISPLAY);
-    delay(2);
-    write_command(0x02);// returb home
-    delay(2);
-
-
+    //lcd_write(CLEAR_DISPLAY,1);
+   
 }
-/*
-int init_display() {
-    const char *device = "/dev/i2c-1";
-    i2c_init(device);
-
-    write_command(0x33);
-    write_command(0x32);
-    write_command(0x06);
-    write_command(0x0C);
-    write_command(0x28);
-    lcd_clear();
-
-  //  lcd_text("Hello, World!", 1, LEFT);
-  //  lcd_text("This is line 2", 2, CENTER);
-
-
-    return 0;
-}*/
--- teletexter.c
+++ teletexter.c
@@ -1,3 +1,28 @@
+/******************************************************************************
+ *                                                                            *
+ *                               TeleTexter                                   *
+ *                                                                            *
+ *                Developed by: Alexander Blohme                              *
+ *                Initial Release: 2024                                       *
+ *                                                                            *
+ *   teletexter.c - The program itself                                        *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the               *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.      *
+ *                                                                            *
+ ******************************************************************************/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,7 +35,8 @@
 #define MAX_CLIENTS 10
 #define BUFFER_SIZE 1024
 
-typedef struct {
+typedef struct 
+{
     int socket;
     char nickname[50];
 } client_t;
@@ -18,7 +44,8 @@ typedef struct {
 pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 client_t *clients[MAX_CLIENTS];
 
-typedef struct {
+typedef struct 
+{
     char command[10];
     void (*function)(client_t *, char *);
 } command_t;
@@ -28,16 +55,33 @@ void handle_ident(client_t *cli, char *args);
 void handle_msg(client_t *cli, char *args);
 void handle_client(client_t *cli);
 
+void strip_newline(char *str) 
+{
+    char *p = str;
+    while (*p) {
+        if (*p == '\r' || *p == '\n') 
+        {
+            *p = '\0';
+            break;
+        }
+        p++;
+    }
+}
+
+
 // Kommandoer
 command_t commands[] = {
     {"IDENT", handle_ident},
     {"MSG", handle_msg},
     {"", NULL}
 };
-void add_client(client_t *cli) {
+void add_client(client_t *cli) 
+{
     pthread_mutex_lock(&clients_mutex);
-    for (int i = 0; i < MAX_CLIENTS; ++i) {
-        if (clients[i] == NULL) {
+    for (int i = 0; i < MAX_CLIENTS; ++i) 
+    {
+        if (clients[i] == NULL) 
+        {
             clients[i] = cli;
             break;
         }
@@ -47,8 +91,10 @@ void add_client(client_t *cli) {
 
 void remove_client(client_t *cli) {
     pthread_mutex_lock(&clients_mutex);
-    for (int i = 0; i < MAX_CLIENTS; ++i) {
-        if (clients[i] == cli) {
+    for (int i = 0; i < MAX_CLIENTS; ++i) 
+    {
+        if (clients[i] == cli) 
+        {
             clients[i] = NULL;
             break;
         }
@@ -58,23 +104,28 @@ void remove_client(client_t *cli) {
     free(cli);
 }
 
-void *client_handler(void *arg) {
+void *client_handler(void *arg) 
+{
     client_t *cli = (client_t *)arg;
     handle_client(cli);
     remove_client(cli);
     return NULL;
 }
 
-void handle_client(client_t *cli) {
+void handle_client(client_t *cli) 
+{
     char buffer[BUFFER_SIZE];
     int nbytes;
-    while ((nbytes = recv(cli->socket, buffer, sizeof(buffer), 0)) > 0) {
+    while ((nbytes = recv(cli->socket, buffer, sizeof(buffer), 0)) > 0) 
+    {
         buffer[nbytes] = '\0';
         char *cmd = strtok(buffer, " ");
         char *args = strtok(NULL, "\0");
 
-        for (int i = 0; commands[i].command[0] != '\0'; ++i) {
-            if (strcmp(commands[i].command, cmd) == 0) {
+        for (int i = 0; commands[i].command[0] != '\0'; ++i) 
+        {
+            if (strcmp(commands[i].command, cmd) == 0) 
+            {
                 commands[i].function(cli, args);
                 break;
             }
@@ -82,28 +133,36 @@ void handle_client(client_t *cli) {
     }
 }
 
-void handle_ident(client_t *cli, char *args) {
-    if (args != NULL) {
+void handle_ident(client_t *cli, char *args) 
+{
+    if (args != NULL) 
+    {
+        
         strncpy(cli->nickname, args, sizeof(cli->nickname) - 1);
         cli->nickname[sizeof(cli->nickname) - 1] = '\0';
-        printf("Client identified as: %s\n", cli->nickname);
+        strip_newline(cli->nickname);   // just for good measure! ;)
+        printf("Client identified as: '%s'\n", cli->nickname);
     }
 }
 
-void handle_msg(client_t *cli, char *args) {
-    if (strlen(cli->nickname) == 0) {
+void handle_msg(client_t *cli, char *args) 
+{
+    if (strlen(cli->nickname) == 0) 
+    {
         char *error_msg = "No nickname is set! Please IDENT first!\n";
         send(cli->socket, error_msg, strlen(error_msg), 0);
         return;
     }
 
-    if (args != NULL) {
+    if (args != NULL) 
+    {
+        strip_newline(args);  // Fjern \r\n fra args
+        
         printf("Message from %s: %s\n", cli->nickname, args);
         lcd_clear();
-        delay(2);
-        lcd_text(cli->nickname,1,LEFT);
-        delay(2);
-        lcd_text(args,2,LEFT);
+        
+        lcd_text(cli->nickname, 1, LEFT);
+        lcd_text(args, 2, LEFT);
         
     }
 }
@@ -129,14 +188,18 @@ int main()
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
-    lcd_text("SysMsg v0.1",1,LEFT);
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    lcd_text("TeleTexter v0.1",1,LEFT);
+    lcd_text("By A. Blohme",2,CENTER);
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) 
+    {
         perror("Bind failed");
+        lcd_text("Sockets failed!",2,CENTER);
         close(server_socket);
         return EXIT_FAILURE;
     }
 
-    if (listen(server_socket, 3) < 0) {
+    if (listen(server_socket, 3) < 0) 
+    {
         perror("Listen failed");
         close(server_socket);
         return EXIT_FAILURE;
@@ -144,9 +207,11 @@ int main()
 
     printf("Server listening on port %d\n", PORT);
 
-    while (1) {
+    while (1) 
+    {
         client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
-        if (client_socket < 0) {
+        if (client_socket < 0) 
+        {
             perror("Accept failed");
             continue;
         }
@@ -156,10 +221,13 @@ int main()
         memset(cli->nickname, 0, sizeof(cli->nickname));
 
         pthread_t tid;
-        if (pthread_create(&tid, NULL, client_handler, (void *)cli) != 0) {
+        if (pthread_create(&tid, NULL, client_handler, (void *)cli) != 0) 
+        {
             perror("Could not create thread");
             free(cli);
-        } else {
+        } 
+        else 
+        {
             add_client(cli);
         }
     }
