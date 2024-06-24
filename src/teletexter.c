6a39b2ac81f420a2cce67e4da71e63734aabf810
Alexander Blohme <alexander@thrino.no>
2024-06-24 19:26:58 +0200

    1: Added better signal handling. 2: Prints host of display on line 2 when starting up

--- src/teletexter.c
+++ src/teletexter.c
@@ -7,40 +7,58 @@
 #include <string.h>
 #include <stdlib.h>
 #include "../include/server/server.h"
+#include "../include/message/message.h"
 #include "../include/config/config.h"
 #include "../include/version.h"
 #include "../include/db/db_handler.h"
 #include "../include/app_args.h"
 #include "../include/log.h"
 
-volatile sig_atomic_t sigint_received = 0;
 
-// Signalhåndteringsfunksjon
-void sigint_handler(int sig)
+void handle_signal(int signal) 
 {
-
-    printf("sigint %d received! Now, HANDLE I-T!\n", sig);
-    sigint_received = 1;
-    #ifndef DISABLE_LCD
-    lcd_text("TeleTexter is",1,CENTER);
-    lcd_text("shutting down...",2,CENTER);
-    
-    #endif
-    
-    close_server();    // properly close the server...
-    // TODO:  close all remainign clients...
+    char past_or_present_tense[20];
+    char action[50];
+    switch (signal)
+    {
+        case SIGTERM:
+            snprintf(past_or_present_tense,sizeof(past_or_present_tense), "TeleTexter was");
+            snprintf(action,sizeof(action), "terminated x(");
+            log_sys_message("[SYSTEM] Service is stopping due to SIGTERM signal");
+        break;
         
-    exit(1);  // ...and get TFO'utta here!
+        case SIGHUP:
+            snprintf(past_or_present_tense,sizeof(past_or_present_tense), "TeleTexter is");
+            snprintf(action,sizeof(action), "restarting...");
+            log_sys_message("[SYSTEM] Service is restarting due to SIGHUP signal");
+        break;
+        
+        case SIGINT:
+            snprintf(past_or_present_tense,sizeof(past_or_present_tense), "TeleTexter was");
+            snprintf(action,sizeof(action), "terminated x(");
+            log_sys_message("[SYSTEM] Service is stopping due to SIGINT signal");
+        break;
+        
+        default:
+            snprintf(past_or_present_tense,sizeof(past_or_present_tense), "TeleTexter recvd");
+            snprintf(action,sizeof(action), "unknown signal!");
+            log_sys_message("[SYSTEM] Service is stopping due to some strange unknown signal in the ether...");
+            
+        break;
+    };
+    lcd_text(past_or_present_tense,1,CENTER);
+    lcd_text(action,2,CENTER);    
+    close_server();
+    exit(0);
 }
 
 
 int main(const int argc, char **argv)
 {
-    struct sigaction sa;
-    sa.sa_handler = sigint_handler;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    sigaction(SIGINT, &sa, NULL);
+    signal(SIGTERM, handle_signal);
+    signal(SIGHUP, handle_signal);
+    signal(SIGINT, handle_signal);
+
 
     printf("\n\n        TeleTexter v%d.%d\n", MAJOR, MINOR);
     printf("Copyright (c) 2024 Alexander Blohme\n-----------------------------------\n\n");
@@ -52,7 +70,14 @@ int main(const int argc, char **argv)
         {
             config.serverConfig.port = arg_srv_port;
         }
+        if (strlen(config.serverConfig.blackListFile) > 0 && strlen(config.serverConfig.whiteListFile) > 0)
+        {
+            log_err_message("Both BlackList and WhiteList has been set in %s! Please remove one of them!", configPath);
+            exit(1);
+        }
+        
     }
+    
     else
     {
         log_sys_message("Exiting...");
@@ -67,9 +92,12 @@ int main(const int argc, char **argv)
 
     // sett velkomstmelding på skjermen
     char welcomeTxt_line1[config.lcdConfig.lcdWidth];
+    char welcometxt_line2[30];
     snprintf(welcomeTxt_line1, sizeof(welcomeTxt_line1), "TeleTexter v%d.%d", MAJOR, MINOR);
-    lcd_text(welcomeTxt_line1, 1, LEFT);
-    lcd_text("By A. Blohme", 2, CENTER);
+    snprintf(welcomeTxt_line2,sizeof(welcomeTxt_line2), "Host: %s", config.serverConfig.serverHost);
+
+    set_line_text(welcomeTxt_line1, 1, "LEFT");
+    set_line_text(welcomeTxt_line2, 2, "CENTER");
 #endif
 
     return start_server();
